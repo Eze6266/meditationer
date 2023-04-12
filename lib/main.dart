2538a417db74efb7184fd03b91779f20cc0b2d47
Emmanuel Ezejiobi <eze6266@gmail.com>
2023-04-12 15:32:00 +0100

    added the flutter native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app/Screens/OnboardingScreen.dart';
 import 'package:provider/provider.dart';
 
 import 'Screens/SplashScreen.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SplashScreen(),
+        home: OnBoardingScreen(),
       ),
     );
   }
