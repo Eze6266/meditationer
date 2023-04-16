2b8727f3f6690b9ca3e143e84f2c66e95d186366
Francis <ijenebefrancis@gmail.com>
2023-04-16 18:44:44 +0100

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:provider/provider.dart';
 
 import 'Screens/SplashScreen.dart';
 import 'utils/navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     ChangeNotifierProvider(
       create: (_) => Navigation(),
@@ -32,3 +37,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// ...
+
